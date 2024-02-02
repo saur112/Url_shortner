01e69af52fbc3037901c7d828e74dbf26feb8381
paras <parasguptapg890@gmail.com>
2024-02-02 20:10:05 +0530

    commit

---
+++ url_shortner/myapp/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+urlpatterns = [
+
+    path('',views.task),
+    path('analytic/',views.analytic),
+    path('<slug:short_url>',views.redirect_url)
+    ]
