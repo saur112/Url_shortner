2333908f77ade199808a4178758cf3133b65fdf1
Paras Gupta <parasguptapg890@gmail.com>
2024-02-24 13:28:01 +0530

    commit

--- url_shortner/settings.py
+++ url_shortner/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'byvjgsj_^ttie7f=85&#+ei*tta*yu3=j%%$06dcd**6z(d$r9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,5 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'myapp/static/')
+STATICFILES_DIRS=(os.path.join(BASE_DIR,'static'))
